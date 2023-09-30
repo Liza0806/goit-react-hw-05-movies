224bf03b503c41dad7ab79814de53714396ecf4e
Liza0806 <126647343+Liza0806@users.noreply.github.com>
2023-09-30 14:41:57 +0300

    hz

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -41,7 +41,14 @@ useEffect(() => {
             on={['hover', 'focus']}
             closeOnDocumentClick
           >
-              <span> {film.poster_path? (<img src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} alt={film.title ?? film.name} width={"200"}/>): (
+              <span 
+              style={{ position: "fixed",
+                       top: "50%", 
+                       left: "50%", 
+                       transform: "translate(-50%, -50%)",
+                       zIndex: 9999, 
+                      }}> 
+    {film.poster_path? (<img src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} alt={film.title ?? film.name} width={"300"}/>): (
   <FcCamcorderPro style={{ width: '100px', height: '100px' }} />
 )}</span>
           </Popup>   
--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -58,13 +58,19 @@ const UpdateQueryString = (evt) =>{
 <SearchResultList>
         {movieData && movieData.map((film) => (
      
-      <SearchResultItem key={film.id}>   <Popup
-      trigger={ open => ( <Link to={`/${film.id}`} state ={{ from: location}} key={film.id}>{film.title}</Link> )}
+      <SearchResultItem key={film.id}>   
+      <Popup trigger={ open => ( <Link to={`/${film.id}`} state ={{ from: location}} key={film.id}>{film.title}</Link> )}
       position="right center"
       on={['hover', 'focus']}
       closeOnDocumentClick
     >
-      <span> {film.poster_path? (<img src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} alt={film.title} width={"200"}/>): (
+      <span  style={{ position: "fixed",
+                       top: "50%", 
+                       left: "50%", 
+                       transform: "translate(-50%, -50%)",
+                       zIndex: 9999, 
+                     }}> 
+    {film.poster_path? (<img src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} alt={film.title} width={"200"}/>): (
   <FcCamcorderPro style={{ width: '100px', height: '100px' }} />
 )}</span>
     </Popup>  </SearchResultItem>
